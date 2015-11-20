dd9991d6de997c4b361caa2e4717cc277e05059c
Caleb Leung <dvcs@calebleung.com>
2015-11-20 11:37:28 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Generate Tumblr Photoset
+
+For use on individual images that you'd like to create a "photoset" modal slideshow... thing.
+
+## Usage
+
+* Add the JavaScript from main.js to the bottom of your Tumblr page
+* Add the 'zetlightbox' class to your desired images.
+
+If you wish to make the call to the actual `Tumblr.Lightbox` in a different manner, don't forget to comment out `linkImages()`
---
+++ main.js
@@ -0,0 +1,26 @@
+var lightboxPhotoset = [];
+
+var lightboxImages = document.getElementsByClassName('zetlightbox');
+
+function populateLightbox(img) {
+    lightboxImage = {
+    
+    "width" : img.naturalWidth,
+    "height" : img.naturalHeight,
+    "low_res" : img.src,
+    "high_res" : img.src,
+    
+    };
+    
+    lightboxPhotoset.push( lightboxImage );
+}
+
+function linkImages(img, index) {
+    img.setAttribute("onClick", "Tumblr.Lightbox.init( lightboxPhotoset, " + (index + 1) + " );");
+    img.setAttribute("style", "cursor:pointer;");
+}
+
+for (var i = 0; i < lightboxImages.length; i++) {
+    populateLightbox( lightboxImages[i] );
+    linkImages( lightboxImages[i], i );
+}
